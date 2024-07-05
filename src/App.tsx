fac54cabad008665cb768243c5a97af1e64629e8
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-07-05 08:37:01 +0200

    add example usage for fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [data, setData] = useState(null);
 
-  return (
-    <>
-      <h1 className="text-3xl text-amber-300">Vite + React + Tailwind</h1>
-      <br/>
-        <button className="border-2 border-b-blue-500" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-    </>
-  )
+    useEffect(() => {
+        fetch('https://dhbw.radicalsimplicity.com/calendar/test/events/8257')
+            .then(response => response.json())
+            .then(json => setData(json))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <>
+            <h1 className="text-3xl text-amber-300">Vite + React + Tailwind</h1>
+            <br/>
+            <button className="border-2 border-b-blue-500" onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <div>
+                <pre>{data ? JSON.stringify(data, null, 2) : "Loading"}</pre>
+            </div>
+        </>
+    )
 }
 
 export default App
