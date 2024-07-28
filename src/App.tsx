b3a87c7e6c7c7cb888d1bd5a3757ff076d0993ef
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2024-07-28 16:38:51 +0200

    Added: Basic Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import {useEffect, useState} from 'react'
+import {Header} from './header/components/Header.tsx'
 import './App.css'
-import {Event} from "./types.ts";
 
 function App() {
-    const [count, setCount] = useState(0)
-    const [data, setData] = useState<Event | null>(null);
-
-    useEffect(() => {
-        fetch('https://dhbw.radicalsimplicity.com/calendar/test/events/8257')
-            .then(response => response.json())
-            .then((json: Event) => setData(json))
-            .catch(error => console.error(error));
-    }, []);
-
     return (
         <>
-            <h1 className="text-3xl text-amber-300">Vite + React + Tailwind</h1>
-            <br/>
-            <button className="border-2 border-b-blue-500" onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
-            <div>
-                <pre>Event: {data ? data.title : "Loading."}</pre>
-            </div>
+            <Header />
         </>
     )
 }
