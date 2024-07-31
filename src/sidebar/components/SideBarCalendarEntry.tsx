b9966c0424d18e2d132fbc3035ec945e44645122
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-07-31 22:11:09 +0200

    added SideBarCalendarEntry

---
+++ src/sidebar/components/SideBarCalendarEntry.tsx
@@ -0,0 +1,3 @@
+//Component that displays a single date entry in the sidebar calendar
+// replacement to the functions NonDay/CurrDay/TodayDay
+// can contain props for the date to save it and access when clicked.
