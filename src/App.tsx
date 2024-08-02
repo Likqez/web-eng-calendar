1f8893a9ac2eca852d7ba5595455a79814aa6134
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2024-08-02 11:44:26 +0200

    Update: Header Sidebar toggle

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {SidebarCalendarProps} from "./sidebar/components/SidebarCalendar.tsx";
 function App() {
     const [calendarDate, setCalendarDate] = useState(new Date()) // State for the main calendar showing the weekly view
     const [sideBarCalendarDate, setSideBarCalendarDate] = useState(new Date()) // State for the sidebar calendar showing the monthly view
+    const [sideBarCalendarVisible, setSideBarVisibility] = useState<boolean>(true)
 
     const sidebarCalendarProp: SidebarCalendarProps = {
         displayDate: calendarDate,
@@ -17,6 +18,7 @@ function App() {
 
     return (
         <>
+            <Header visible={sideBarCalendarVisible} onMenuToggle={setSideBarVisibility}/>
             <Sidebar visible={sideBarCalendarVisible} sidebar={sidebarCalendarProp}/>
             <br/><br/><br/>
         </>
--- src/header/components/Header.tsx
+++ src/header/components/Header.tsx
@@ -2,12 +2,19 @@
 import {GiHamburgerMenu} from "react-icons/gi";
 import {IoSettingsSharp} from "react-icons/io5";
 
-const Header = () => {
+interface HeaderProps {
+    visible: boolean
+    onMenuToggle: (vis: boolean) => void;
+}
+
+const Header = (props: HeaderProps) => {
     return (
         <>
             <div id="header" className="flex flex-row h-14 border-b-2 border-gray-400 min-w-72">
                 {/* Sidebar menu button */}
-                <GiHamburgerMenu className="flex-none fill-black py-2 w-fit h-full ml-2 my-auto"/>
+                <button id="menu_button" onClick={(_) => {props.onMenuToggle(!props.visible)}}>
+                    <GiHamburgerMenu className="flex-none fill-black py-2 w-fit h-full ml-2 my-auto"/>
+                </button>
 
                 {/* Settings options */}
                 <IoSettingsSharp className="flex-none fill-black py-2 w-fit h-full ml-auto mr-2 my-auto"/>
