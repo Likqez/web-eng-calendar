6b97951e89321647d0b68ba54b448fac19195597
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-08-08 21:44:41 +0200

    update header remover burger

--- src/header/components/Header.tsx
+++ src/header/components/Header.tsx
@@ -1,5 +1,4 @@
 // oshea-02.08.2024: Hamburger, cheeseburger, bigmac, whopper
-import {GiHamburgerMenu} from "react-icons/gi";
 import {IoSettingsSharp} from "react-icons/io5";
 
 interface HeaderProp {
@@ -10,11 +9,6 @@ const Header = (props: HeaderProp) => {
     return (
         <>
             <div id="header" className="flex flex-row h-14 border-b-2 border-gray-400 min-w-72">
-                {/* Sidebar menu button */}
-                <button id="menu_button">
-                    <GiHamburgerMenu className="flex-none fill-black py-2 w-fit h-full ml-2 my-auto"/>
-                </button>
-
                 {/* Settings options */}
                 <IoSettingsSharp className="flex-none fill-black py-2 w-fit h-full ml-auto mr-2 my-auto"/>
 
