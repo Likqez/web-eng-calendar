b513fb2a6bb0b11a9e4434591b6b40886fa3abe9
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2024-08-08 10:12:23 +0200

    Added: More Date util funcs

--- src/businesslogic/util/DateUtil.ts
+++ src/businesslogic/util/DateUtil.ts
@@ -1,3 +1,67 @@
+// Because working with Dates is such a massive pain
+
+/**
+ * @param o1 Date object
+ * @param o2 Date object
+ * @returns 0 if they are equal, 1 if o1 is later than o2, else -1
+ */
+export const compareDatesWithTime = (o1: Date, o2: Date): number => {
+    if (o1.getTime() == o2.getTime())   return 0;
+    if (o1.getTime() > o2.getTime())    return 1;
+    return -1;
+}
+
+/**
+ * Ignores time
+ * @param o1 Date object
+ * @param o2 Date object
+ * @returns 0 if they are equal, 1 if o1 is later than o2, else -1
+ */
+export const compareDates = (o1: Date, o2: Date): number => {
+    const nto1 = new Date(o1), nto2 = new Date(o2);
+    nto1.setHours(0, 0, 0, 0);
+    nto2.setHours(0, 0, 0, 0);
+    return compareDatesWithTime(nto1, nto2);
+}
+
+/**
+ * Ignores time
+ * @param o1 Date object
+ * @param o2 Date Object
+ * @returns True, if the dates are the same
+ */
 export const isSameDate = (o1: Date, o2: Date): boolean => {
-    return o1.getFullYear() === o2.getFullYear() && o1.getMonth() === o2.getMonth() && o1.getDate() === o2.getDate();
+    return compareDates(o1, o2) === 0;
+}
+
+/**
+ * @param start Start date
+ * @param end   End date
+ * @returns A list of dates between "start" and "end"
+ */
+export const calcDaysFromTo = (start: Date, end: Date): [Date] => {
+    if (compareDates(start, end) === 1) throw new Error("The end date is eralier than the start date!");
+
+    const dates: [Date] = [ start ];
+    for (let i = 1;; i++) {
+        const newDate = new Date(start.getFullYear(), start.getMonth(), start.getDate() + i);
+        if (compareDates(newDate, end)) break;
+        dates.push(newDate);
+    }
+
+    return dates;
+}
+
+/**
+ * Ignores time
+ * @param date Date to be converted
+ * @returns An integer representing this date
+ */
+export const calcDateIndex = (date: Date): number => {
+    // yyyyMMdd as a num
+    const yearStr = date.getFullYear().toString();
+    const monthStr = "00".substring(0, 2 - date.getMonth().toString().length) + date.getMonth().toString();
+    const dateStr = "00".substring(0, 2 - date.getDate().toString().length) + date.getDate().toString();
+
+    return Number.parseInt(yearStr + monthStr + dateStr);
 }
