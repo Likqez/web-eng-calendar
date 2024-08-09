3c4924e031476ac166301c92aef24b888c2da638
Lukas Lanzner <LukasLanzner@dotspace.dev>
2024-08-09 18:52:30 +0200

    feat: implemented auto scroll based on current time

--- src/calendar/components/Calendar.tsx
+++ src/calendar/components/Calendar.tsx
@@ -2,6 +2,8 @@ import '../styling/Calendar.css'
 import {CalendarEvent} from "../../businesslogic/types.ts";
 import CalendarHeader from './CalendarHeader.tsx';
 import CalendarBody from './CalendarBody.tsx';
+import {useEffect, useRef} from "react";
+import {ENTRY_HEIGHT} from "./CalendarVisualGrid.tsx";
 
 interface CalendarProps {
     events: CalendarEvent[];
@@ -13,12 +15,23 @@ interface CalendarProps {
 
 const Calendar = (props: CalendarProps) => {
     const weekDates: Date[] = calcWeekDates(props.selectedDate);
+    const calendarBodyRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (calendarBodyRef.current) {
+            calendarBodyRef.current.scrollTo({
+                top: calendarBodyRef.current.scrollHeight - (ENTRY_HEIGHT * (25 - new Date().getHours())),
+                behavior: "smooth"
+            });
+        }
+    }, []);
+
 
     return (
         <>
             <div id="calendar" className="flex flex-col w-full pt-2 overflow-hidden">
                 <CalendarHeader weekDates={weekDates} selectedDate={props.selectedDate} />
-                <div className="overflow-y-scroll scrollbar-hide pb-6">
+                <div ref={calendarBodyRef} className="overflow-y-scroll scrollbar-hide pb-6">
                     <CalendarBody
                             weekDates={weekDates} 
                             events={props.events}
